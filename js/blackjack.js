cb54720da835bc74f9d0f32aaf435233e44debd6
Jeremy Koulish <jkoulish@gmail.com>
2015-07-14 09:00:42 -0400

    values tied to card elements

--- js/blackjack.js
+++ js/blackjack.js
@@ -2,18 +2,21 @@ var mainPlayer;
 var player = ["", ""];
 var dealer;
 
-stackMakeDeck(1);
+stackMakeDeck(3);
 stackShuffle(3);
 
+
 $("#deal").click(function startGame(){
-var card = stackDeal();
   $("#dcards").append("<div id='dealerone'></div>");
   $("#dealerone").addClass("card");
-  $("div#dealerone").val(console.log(card))
+  $("div#dealerone").html(cards[0])
   $("#dcards").append("<div id='dealertwo'></div>").addClass("card");
   $("#dealertwo").addClass("card");
+  $("div#dealertwo").html(cards[1])
   $("#pcards").append("<div id='playerone'></div>");
   $("#playerone").addClass("card");
+  $("div#playerone").val(console.log(cards[2]))
   $("#pcards").append("<div id='playertwo'></div>");
   $("#playertwo").addClass("card");
+  $("div#playertwo").val(console.log(cards[3]))
 })
