26044c38e8661d6b7e6eb242bb380ae76ae5c3bb
Jeremy Koulish <jkoulish@gmail.com>
2015-07-14 09:58:00 -0400

    cards appear on screen

--- js/blackjack.js
+++ js/blackjack.js
@@ -7,16 +7,12 @@ stackShuffle(3);
 
 
 $("#deal").click(function startGame(){
-  $("#dcards").append("<div id='dealerone'></div>");
-  $("#dealerone").addClass("card");
-  $("div#dealerone").html(cards[0])
+  $("#dcards").append("<div id='dealerone'></div>").addClass("card");;
+  $("div#dealerone").html(cards[0].createNode());
   $("#dcards").append("<div id='dealertwo'></div>").addClass("card");
-  $("#dealertwo").addClass("card");
-  $("div#dealertwo").html(cards[1])
+  $("div#dealertwo").html(cards[1].createNode())
   $("#pcards").append("<div id='playerone'></div>");
-  $("#playerone").addClass("card");
-  $("div#playerone").val(console.log(cards[2]))
+  $("div#playerone").html(cards[2].createNode()).addClass("card");;
   $("#pcards").append("<div id='playertwo'></div>");
-  $("#playertwo").addClass("card");
-  $("div#playertwo").val(console.log(cards[3]))
+  $("div#playertwo").html(cards[3].createNode()).addClass("card");;
 })
